3533fcb7f1c6ff091390c2c6381cccd054470c43
igorlakovsky <igorlakovsky@yandex.ru>
2023-11-08 14:31:53 +0400

    pagination + page query params

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,12 @@
 import axios from 'axios'
 
 import { useRouter, useRoute } from 'vue-router'
-import { ref, watchEffect } from 'vue'
+import { ref, watchEffect, watch } from 'vue'
 
 import SearchCard from './components/SearchCard.vue'
-import Spinner from './components/Spinner.vue'
 import RepoCard from './components/RepoCard.vue'
+import Spinner from './components/Spinner.vue'
+import Pagination from './components/Pagination.vue'
 
 type ResponceData = {
   total_count: number
@@ -39,11 +40,10 @@ const searchData = ref<SearchData>()
 const searchTimer = ref<number>()
 const searchDelay = 500
 
-const isLoading = ref(false)
+const page = ref<number>(1)
+const perPage = ref<number>(10)
 
-const getRequestQ = (url: string) => {
-  return url.slice(url.indexOf('?q=') + 3)
-}
+const isLoading = ref(false)
 
 const getRequestData = (data: ResponceData) => {
   return {
@@ -61,12 +61,18 @@ const getRequestData = (data: ResponceData) => {
   }
 }
 
-async function fetchData(repoName: string) {
+async function fetchData(repoName: string, pageNumber: number, perPageCount: number) {
   isLoading.value = true
-  const responce = await axios.get(`https://api.github.com/search/repositories?q=${repoName}`)
+  const responce = await axios.get(`https://api.github.com/search/repositories`, {
+    params: { q: repoName, page: pageNumber, per_page: perPageCount },
+  })
 
   // Проверяем соответствие ответа с сервера и текущему поиску
-  if (getRequestQ(responce.config.url!) === searchText.value) {
+  if (
+    responce.config.params.q === searchText.value &&
+    responce.config.params.page === page.value &&
+    responce.config.params.per_page === perPage.value
+  ) {
     searchData.value = getRequestData(responce.data)
     isLoading.value = false
 
@@ -74,12 +80,14 @@ async function fetchData(repoName: string) {
       name: '',
       query: {
         q: searchText.value,
+        page: page.value,
+        per_page: perPage.value,
       },
     })
   }
 }
 
-watchEffect(() => {
+watch([searchText, page, perPage], () => {
   // Отменяем таймер запроса если поиск изменился
   if (searchTimer.value) {
     clearTimeout(searchTimer.value)
@@ -89,7 +97,7 @@ watchEffect(() => {
   // Запускаем таймер с запросом
   if (searchText.value) {
     searchTimer.value = setTimeout(() => {
-      fetchData(searchText.value)
+      fetchData(searchText.value, page.value, perPage.value)
     }, searchDelay)
   } else {
     // Если пользователь очистил строку поиска
@@ -108,6 +116,12 @@ watchEffect(() => {
   if (route.query.q) {
     searchText.value = route.query.q as string
   }
+  if (route.query.page) {
+    page.value = Number(route.query.page as string)
+  }
+  if (route.query.per_page) {
+    perPage.value = Number(route.query.per_page as string)
+  }
 })
 </script>
 
@@ -115,20 +129,22 @@ watchEffect(() => {
   <section class="py-12 px-4 min-h-screen bg-gray-50 flex flex-col gap-6">
     <SearchCard v-model="searchText"></SearchCard>
     <Spinner v-if="isLoading"></Spinner>
-    <div
-      v-else-if="searchData?.items.length"
-      class="w-full gap-4 2xl:max-w-7xl 2xl:grid-cols-3 xl:max-w-5xl lg:max-w-4xl md:max-w-2xl md:grid-cols-2 max-w-lg mx-auto grid grid-cols-1"
-    >
-      <RepoCard
-        v-for="(repo, index) in searchData.items"
-        :key="index"
-        :author="repo.author"
-        :name="repo.name"
-        :description="repo.description"
-        :language="repo.language"
-        :star-count="repo.star_count"
-        :avatar="repo.avatar"
-      ></RepoCard>
+    <div v-else-if="searchData?.items.length">
+      <div
+        class="w-full gap-4 2xl:max-w-7xl 2xl:grid-cols-3 xl:max-w-5xl lg:max-w-4xl md:max-w-2xl md:grid-cols-2 max-w-lg mx-auto grid grid-cols-1"
+      >
+        <RepoCard
+          v-for="(repo, index) in searchData.items"
+          :key="index"
+          :author="repo.author"
+          :name="repo.name"
+          :description="repo.description"
+          :language="repo.language"
+          :star-count="repo.star_count"
+          :avatar="repo.avatar"
+        ></RepoCard>
+      </div>
+      <Pagination v-model:page="page" v-model:per-page="perPage" :total-count="searchData.total_count"></Pagination>
     </div>
     <div v-else-if="searchData?.total_count === 0" class="text-center text-slate-400 font-medium text-sm">No data</div>
   </section>
---
+++ src/components/Pagination.vue
@@ -0,0 +1,100 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+
+interface Props {
+  page: number
+  perPage: number
+  totalCount: number
+}
+
+const props = defineProps<Props>()
+const $emit = defineEmits(['update:page', 'update:perPage'])
+
+const showingMin = computed(() => (props.page - 1) * props.perPage + 1)
+const showingMax = computed(() => props.page * props.perPage)
+
+function onPrevClick() {
+  if (props.page > 1) {
+    $emit('update:page', props.page - 1)
+  }
+}
+
+function onNextClick() {
+  $emit('update:page', props.page + 1)
+}
+</script>
+
+<template>
+  <div class="flex flex-col items-center mt-6">
+    <!-- Buttons -->
+    <div class="inline-flex mt-2 xs:mt-0">
+      <button
+        class="flex items-center justify-center px-3 h-8 text-sm font-medium text-white bg-gray-800 rounded-l hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white disabled:opacity-40 disabled:hover:bg-gray-800 transition-all"
+        :disabled="props.page === 1"
+        @click="onPrevClick"
+      >
+        <svg
+          class="w-3.5 h-3.5 mr-2"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 14 10"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M13 5H1m0 0 4 4M1 5l4-4"
+          />
+        </svg>
+        Prev
+      </button>
+      <button
+        class="flex items-center justify-center px-3 h-8 text-sm font-medium text-white bg-gray-800 border-0 border-l border-gray-700 rounded-r hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+        @click="onNextClick"
+      >
+        Next
+        <svg
+          class="w-3.5 h-3.5 ml-2"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 14 10"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M1 5h12m0 0L9 1m4 4L9 9"
+          />
+        </svg>
+      </button>
+    </div>
+
+    <!-- Info text -->
+    <span class="text-sm text-gray-700 dark:text-gray-400 mt-4">
+      Showing <span class="font-semibold text-gray-900 dark:text-white">{{ showingMin }}</span> to
+      <span class="font-semibold text-gray-900 dark:text-white">{{ showingMax }}</span> of
+      <span class="font-semibold text-gray-900 dark:text-white">{{ totalCount }}</span> Repositories
+    </span>
+
+    <!-- Per page select -->
+    <div>
+      <span class="text-sm text-gray-700 dark:text-gray-400"> Show per page </span>
+      <select
+        name="select"
+        class="text-xs"
+        :value="perPage"
+        @input="$emit('update:perPage', Number(($event.target as HTMLInputElement).value))"
+      >
+        <option value="10">10</option>
+        <option value="20">20</option>
+        <option value="30">30</option>
+        <option value="50">50</option>
+        <option value="100">100</option>
+      </select>
+    </div>
+  </div>
+</template>
